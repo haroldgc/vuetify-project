128c4261bea5a13683f72231c8a9313f2e5d5785
haroldgc <harold.gc@gmail.com>
2023-10-14 20:16:54 +0200

    Init commit

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar-title>
+      Chess
+    </v-app-bar-title>
+  </v-app-bar>
+
+  <v-navigation-drawer app v-model="drawer">
+    <v-list-item title="Chess" subtitle="Chess"></v-list-item>
+    <v-divider></v-divider>
+    <v-list-item link title="List Item 1"></v-list-item>
+    <v-list-item link title="List Item 2"></v-list-item>
+    <v-list-item link title="List Item 3"></v-list-item>
+  </v-navigation-drawer>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(false)
+</script>
